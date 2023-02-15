7ee206f1dca85ec66f6caf297edc18ff1c5f3597
Mitchell Bower <mitchell@bowermail.net>
2023-02-15 01:18:03 -0700

    added threading to db word retrieval

--- watchwords_app/views.py
+++ watchwords_app/views.py
@@ -573,6 +573,7 @@ class Fireman:
 
 from random import randint
 from random import shuffle
+import threading as t
 
 class Comissioner:
     """The Comissioner provides functionallity for choosing watchwords from the database and assigning them colors for gameplay.
@@ -597,16 +598,39 @@ class Comissioner:
         self.watchwords = [] # reset watchwords
         results = []
         self._decide_words(collections, fireman)
+        threads = []
+        lock = t.Lock()
+        
         for i in collections:
             for j in self.watchword_ids[i]:
-                data = fireman.get_data(i, j)
-                while data in results:
-                    data = fireman.get_data(i, self._get_new_word_id(i, fireman))
-                results.append(data)
+                threads.append(t.Thread(target=(self._get_threaded_data), args=(i, j, lock, results, fireman)))
+                # data = fireman.get_data(i, j)
+                # while data in results:
+                #     data = fireman.get_data(i, self._get_new_word_id(i, fireman))
+                # results.append(data)
+        for i in threads:
+            i.start()
+        for i in threads:
+            i.join()
+        
         shuffle(results)
         self.watchwords = results
         self._generate_color_word_key()
         self.watchword_ids = {} # reset watchword_ids for future use
+    
+    def _get_threaded_data(self, col, id, lock, results, fireman):
+        """Has the thread take put the data in the results list. If the data is already in the results list, thread will get new data."""
+        data = fireman.get_data(col, id)
+        adding_needed = True
+        while adding_needed:
+            lock.acquire()
+            if not (data in results):
+                results.append(data)
+                lock.release()
+                adding_needed = False
+            else:
+                lock.release()
+                data = fireman.get_data(col, self._get_new_word_id(col, fireman))
 
     def _decide_words(self, collections, fireman):
         """gets 25 watchwords IDs for the game. It trys to get at least one from each collection
@@ -640,7 +664,8 @@ class Comissioner:
                         is_trying = False
                     else:
                         is_trying = True
-        
+    
+    
     def _get_new_word_id(self, collection, fireman):
         """gets a new watchword ID that is not already in the member list: watchword_ids[collection]
 
@@ -696,13 +721,6 @@ class Comissioner:
 
 
 
-
-
-
-# ===========================================================================
-#                                   PRIVATE DATA:
-
-# ===========================================================================
 
 
 
