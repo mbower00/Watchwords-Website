96ab634e38c5de1e7b9ef9765e969a8040ae9b4d
Sjskyler27desk <Sjskyler27@gmail.com>
2023-03-27 11:50:03 -0600

    launchps1pluscursor

---
+++ launch.ps1
@@ -0,0 +1,5 @@
+Start-Process "C:\Program Files\Google\Chrome\Application\chrome.exe" "http://127.0.0.1:8000/"
+& cd "C:\Users\sky\OneDrive\Documents\Programming\watchwords2!\Watchwords-Website-master"
+& py manage.py runserver
+# C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoExit -ExecutionPolicy Bypass -File "C:\Users\sky\OneDrive\Documents\Programming\watchwords2!\Watchwords-Website-master\launch.ps1"
+# use this for a one click execution
